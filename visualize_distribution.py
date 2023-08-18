830b5ccd12b506383c68bc67f662a141beb0f0a4
wagenrace <nijhof.dns@gmail.com>
2023-08-18 13:32:08 +0200

    normalize data and label axis

--- visualize_distribution.py
+++ visualize_distribution.py
@@ -10,5 +10,8 @@ heatmap = np.zeros((10, 10))
 for sample in all_samples:
     for index, number in enumerate(sample):
         heatmap[index][number] += 1
+heatmap /= len(all_samples)
+plt.ylabel("New position")
+plt.xlabel("Start position")
 plt.imshow(heatmap)
 plt.show()
