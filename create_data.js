aa42a37805f898e79fbc148bb2881ab93807aaa5
wagenrace <nijhof.dns@gmail.com>
2023-08-18 13:08:29 +0200

    more data

--- create_data.js
+++ create_data.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 let data = []
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 100000; i++) {
     data.push([0, 1, 2, 3, 4, 5, 6, 7, 8, 9].sort(() => Math.random() - 0.5))
 }
 
